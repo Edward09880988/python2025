36575f77d3b905d2afe9193aa38d521b19edc11b
Edward09880988 <st110222@chps.tp.edu.tw>
2025-10-27 21:02:51 +0800

    Update ex1-1.py

--- ex1-1.py
+++ ex1-1.py
@@ -1,1 +1,15 @@
- print ("Hello World") 
+'''
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+'''
+
+
+
+print("Hello World")
+    
+if 5>2:
+    print("5比2大")
+A="112"
+print(A)
