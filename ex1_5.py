4e63f1de0d492cab6fc46c1c0f3ce411b70c29c9
Edward09880988 <st110222@chps.tp.edu.tw>
2025-11-10 20:14:07 +0800

    Update ex1_5.py

--- ex1_5.py
+++ ex1_5.py
@@ -10,3 +10,22 @@ print(f"零：{zero}")
 
 big_number = 123456789012345678901234567890
 print(f"大整數：{big_number}")
+
+
+
+
+
+
+
+
+# 數字運算範例
+a = int(input("數字一"))
+b = int(input("數字二"))
+
+print(f"加法：{a} + {b} = {a + b}")
+print(f"減法：{a} - {b} = {a - b}")
+print(f"乘法：{a} * {b} = {a * b}")
+print(f"除法：{a} / {b} = {a / b}")
+print(f"整數除法：{a} // {b} = {a // b}")
+print(f"餘數：{a} % {b} = {a % b}")
+print(f"次方：{a} ** {b} = {a ** b}")
